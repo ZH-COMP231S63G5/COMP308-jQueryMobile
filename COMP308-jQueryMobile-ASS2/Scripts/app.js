30b21b07ebaa39219455a3ab28f791142ac895d2
JohnnyHu9 <r17d09@gmail.com>
2015-02-18 23:29:10 -0500

    add project slider in home page

--- COMP308-jQueryMobile-ASS2/Scripts/app.js
+++ COMP308-jQueryMobile-ASS2/Scripts/app.js
@@ -2,3 +2,25 @@
     console.log("mobile start");
 
 }
+
+
+//http://owlgraphic.com/owlcarousel/index.html
+$(document).ready(function () {
+
+    $("#owl-demo").owlCarousel({
+        autoPlay: 2000,
+        navigation: true, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true
+
+        // "singleItem:true" is a shortcut for:
+        // items : 1, 
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+
+    });
+
+});
